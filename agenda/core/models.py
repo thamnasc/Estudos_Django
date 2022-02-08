b676089d7b83c271f58fe40edb30e9abb5adc402
thamnasc <thalitanas09@gmail.com>
2022-02-08 00:33:30 -0300

    tabela de eventos com multiplo usuarios e filtros de pesquisa

--- agenda/core/models.py
+++ agenda/core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+
+    #aceita descricao em branco ou nula
+    descricao = models.TextField(blank=True, null=True)
+
+    #verbose_name altera o título para 'Data do Evento'
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+
+    #captura a data que o usuário inseriu o evento no bacno
+    #auto_now insere automaticamente a hora atual, independente do usuário
+    data_criacao = models.DateTimeField(auto_now=True)
+
+    #para criar multiplos usuarios
+    #models.CASCADE --> se for excluido o usuario, exclui também os eventos dele
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    #tabela
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        #sempre que chamar o título, retorna
+        return self.titulo
